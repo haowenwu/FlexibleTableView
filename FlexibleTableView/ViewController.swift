1bf539326ea5fd02654dfae078fc0c07053e15fe
WakeWu <whw629@gmail.com>
2015-07-27 10:09:58 +0800

    first

---
+++ FlexibleTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlexibleTableView
+//
+//  Created by 吴浩文 on 15/7/27.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
